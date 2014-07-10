4d0fc19f85ba46cff7928a60b1d1e51818bff5ee
Sebastian M. Gauck <smg@42ls.de>
2014-07-10 20:15:07 +0200

    add state helpers to invoice

--- lib/skippr/resources/invoice.rb
+++ lib/skippr/resources/invoice.rb
@@ -1,5 +1,21 @@
 module Skippr
   class Invoice < Api
 
+    ACCOUNTED = 0
+    PRINTED = 1
+    DRAFT = 4
+
+    def accounted?
+      state == ACCOUNTED
+    end
+
+    def printed?
+      state <= PRINTED
+    end
+
+    def draft?
+      state == DRAFT
+    end
+
   end
 end
